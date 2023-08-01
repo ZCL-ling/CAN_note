364aadbc4125f0f01ec5da0146dc25f6ed6dad7f
zhangcailing <zhangcailing@lixiang.com>
2023-08-01 10:45:17 +0800

    接收帧模型 和 接收帧视图

--- receivedframesmodel.py
+++ receivedframesmodel.py
@@ -5,6 +5,12 @@ from enum import IntEnum
 
 from PySide6.QtCore import QAbstractTableModel, QModelIndex, QSize, Qt
 
+# QAbstractTableModel 可创建自定义的表格类型
+# QModelIndex 访问和操作表格模型中的数据
+
+
+# 枚举类，每个成员对应表格模型的一个列。
+# 通过为每个列分配一个唯一的整数值，可以在代码中使用这些列进行 索引、访问和控制。
 
 class ReceivedFramesModelColumns(IntEnum):
     number = 0
@@ -18,6 +24,11 @@ class ReceivedFramesModelColumns(IntEnum):
 
 clipboard_text_role = Qt.UserRole + 1
 
+# 列表
+# 对齐方式
+# Qt.AlignRight | Qt.AlignVCenter：右对齐且垂直居中
+# Qt.AlignCenter   水平居中对齐 并 垂直居中
+# Qt.AlignLeft | Qt.AlignVCenter 左对齐且垂直居中
 
 column_alignment = [Qt.AlignRight | Qt.AlignVCenter, Qt.AlignRight | Qt.AlignVCenter,
                     Qt.AlignCenter, Qt.AlignRight | Qt.AlignVCenter,
@@ -28,18 +39,28 @@ class ReceivedFramesModel(QAbstractTableModel):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.m_framesQueue = []  # QQueue()
-        self.m_framesAccumulator = []
-        self.m_queueLimit = 0
+        self.m_framesQueue = []  # QQueue() 用于存储表格模型中的行
+        self.m_framesAccumulator = [] 
+        self.m_queueLimit = 0 # 用于限制 行（也就是队列 Queue）的大小
 
+
+    # 删除指定行数的数据
+    # row:要删除的行的起始索引
+    # count:删除的行数
+    # parent:在模型中，删除行的负索引
     def remove_rows(self, row, count, parent):
-        self.beginRemoveRows(parent, row, row + count - 1)
-        self.m_framesQueue = self.m_framesQueue[0:row] + self.m_framesQueue[row + count:]
-        self.endRemoveRows()
+        self.beginRemoveRows(parent, row, row + count - 1) #开始删除指定行的信号
+        self.m_framesQueue = self.m_framesQueue[0:row] + self.m_framesQueue[row + count:] #切片 左闭右开
+        # 将m_framesQueue列表 切片 为两部分，并将指定行重新赋值为m_framesQueue.这样就删除了指定的行
+        self.endRemoveRows() #删除行的结束信号
         return True
 
+    # 用于返回表格模型中 指定 行 或 列的标题或者尺寸
+    # section: 表示行 或者 列 的索引
+    # orientation： 数据的方向，水平或者垂直
+    # role: 用于请求不同类型数据的角色，例如 显示数据、尺寸提示等
     def headerData(self, section, orientation, role):
-        if role == Qt.DisplayRole and orientation == Qt.Horizontal:
+        if role == Qt.DisplayRole and orientation == Qt.Horizontal:    #水平方向的显示数据，返回相应的标题字符串
             if section == ReceivedFramesModelColumns.number:
                 return "#"
             if section == ReceivedFramesModelColumns.timestamp:
@@ -53,9 +74,9 @@ class ReceivedFramesModel(QAbstractTableModel):
             if section == ReceivedFramesModelColumns.data:
                 return "Data"
 
-        if role == Qt.SizeHintRole and orientation == Qt.Horizontal:
+        if role == Qt.SizeHintRole and orientation == Qt.Horizontal:  # 水平方向的尺寸提示，返回相应的提示
             if section == ReceivedFramesModelColumns.number:
-                return QSize(80, 25)
+                return QSize(80, 25) # 宽度为80像素，高度为25像素
             if section == ReceivedFramesModelColumns.timestamp:
                 return QSize(130, 25)
             if section == ReceivedFramesModelColumns.flags:
@@ -68,11 +89,20 @@ class ReceivedFramesModel(QAbstractTableModel):
                 return QSize(200, 25)
         return None
 
+    # 获取表格模型中指定索引位置的数据，根据角色来返回不同类型的数据
+    # index:获取数据的索引
+    # role：请求不同类型数据的角色，例如：显示数据、对齐方式
     def data(self, index, role):
         if not self.m_framesQueue:
             return None
-        row = index.row()
-        column = index.column()
+        row = index.row() # 获取行号
+        column = index.column() # 获取列号
+        # 根据 role 值 判断请求的数据类型
+        # TextAlignmentRole 返回指定列 的对齐方式，column_alignment是一个列表，存储了不同列的对齐方式。
+        # DisplayRole 返回 具体的显示数据，m_framesQueue列表中对应索引位置的数据
+        # 如果 role 是 clipboard_text_role，则返回剪贴板文本数据，
+        #         对于特定列（ReceivedFramesModelColumns.DLC 列）返回 [数据]，否则返回原始的数据值。
+        # 如果 role 是其他值，则返回 None
         if role == Qt.TextAlignmentRole:
             return column_alignment[index.column()]
         if role == Qt.DisplayRole:
@@ -82,18 +112,55 @@ class ReceivedFramesModel(QAbstractTableModel):
             return f"[{f}]" if column == ReceivedFramesModelColumns.DLC else f
         return None
 
+    """
+    返回表格模型中的行数。
+
+    参数:
+    -父索引有效，则返回 0。
+    - 否则，返回 m_framesQueue 列表的长度。
+    """
     def rowCount(self, parent=QModelIndex()):
         return 0 if parent.isValid() else len(self.m_framesQueue)
 
+    """
+    返回表格模型中的列数。
+
+    参数:
+    - parent: 父索引，默认为无效索引。
+
+    返回值:
+    - 如果父索引有效，则返回 0。
+    - 否则，返回 ReceivedFramesModelColumns 列表的长度。
+    """
     def columnCount(self, parent=QModelIndex()):
         return 0 if parent.isValid() else ReceivedFramesModelColumns.count
 
+    """
+    将 slvector 中的帧数据追加到 m_framesAccumulator 中。
+
+    参数:
+    - slvector: 帧数据序列。
+    """
     def append_frames(self, slvector):
         self.m_framesAccumulator.extend(slvector)
 
+    """
+    返回是否需要更新表格模型。
+
+    返回值:
+    - 如果 m_framesAccumulator 中有积累的数据，则返回 True。
+    - 否则，返回 False。
+    """
     def need_update(self):
         return self.m_framesAccumulator
 
+    """
+    更新表格模型数据。
+
+    如果 m_framesAccumulator 中有积累的数据，则将数据追加到表格模型中，并清空 m_framesAccumulator。
+
+    如果启用了队列限制（m_queueLimit 不为 0），则根据限制进行处理，添加或删除数据行。
+    """
     def update(self):
         if not self.m_framesAccumulator:
             return
@@ -104,15 +171,26 @@ class ReceivedFramesModel(QAbstractTableModel):
             self.append_frames_unlimited(self.m_framesAccumulator)
         self.m_framesAccumulator.clear()
 
+    """
+    将 slvector 中的帧数据追加到表格模型中，以环形缓冲区的方式处理。
+
+    如果帧数量超过了队列限制，根据情况移除多余的数据行。
+
+    参数:
+    - slvector: 帧数据序列。
+    """
     def append_frames_ring_buffer(self, slvector):
         slvector_len = len(slvector)
         row_count = self.rowCount()
+
+        # 如果帧数量超过队列限制，根据情况移除多余的数据行
         if self.m_queueLimit <= row_count + slvector_len:
             if slvector_len < self.m_queueLimit:
                 self.remove_rows(0, row_count + slvector_len - self.m_queueLimit + 1)
             else:
                 self.clear()
 
+        # 在表格模型的末尾插入数据行
         self.beginInsertRows(QModelIndex(), row_count, row_count + slvector_len - 1)
         if slvector_len < self.m_queueLimit:
             self.m_framesQueue.extend(slvector)
@@ -120,21 +198,44 @@ class ReceivedFramesModel(QAbstractTableModel):
             self.m_framesQueue.extend(slvector[slvector_len - self.m_queueLimit:])
         self.endInsertRows()
 
+    """
+    将单个帧数据追加到表格模型中。
+
+    参数:
+    - slist: 单个帧数据。
+    """
     def append_frame(self, slist):
         self.append_frames([slist])
 
+    """
+    将 slvector 中的帧数据追加到表格模型中，不进行队列限制处理。
+
+    参数:
+    - slvector: 帧数据序列。
+    """
     def append_frames_unlimited(self, slvector):
         row_count = self.rowCount()
         self.beginInsertRows(QModelIndex(), row_count, row_count + len(slvector) - 1)
         self.m_framesQueue.extend(slvector)
         self.endInsertRows()
 
+    """
+    清空表格模型数据。
+    """
     def clear(self):
         if self.m_framesQueue:
             self.beginResetModel()
             self.m_framesQueue.clear()
             self.endResetModel()
 
+    """
+    设置队列限制。
+
+    设置 m_queueLimit 的值，并根据新的限制进行处理，移除多余的数据行。
+
+    参数:
+    - limit: 队列限制的值。
+    """
     def set_queue_limit(self, limit):
         self.m_queueLimit = limit
         frame_queue_len = len(self.m_framesQueue)
--- receivedframesview.py
+++ receivedframesview.py
@@ -4,6 +4,8 @@
 from PySide6.QtCore import QPoint, Qt, Slot
 from PySide6.QtGui import QAction, QKeySequence
 from PySide6.QtWidgets import QApplication, QMenu, QTableView
+# QPoint是用于表示平面上的点的类，Qt是Qt框架的核心模块，Slot是一个装饰器，用于声明一个槽函数
+# QAction是用于创建菜单、工具栏和快捷键的动作的类，QKeySequence是用于表示键盘快捷键的类。
 
 from receivedframesmodel import clipboard_text_role
 
@@ -12,28 +14,42 @@ class ReceivedFramesView(QTableView):
 
     def __init__(self, parent):
         super().__init__(parent)
-        self.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.customContextMenuRequested.connect(self._context_menu)
+        self.setContextMenuPolicy(Qt.CustomContextMenu) # 设置表格视图的上下文菜单策略为Qt.CustomContextMenu
+        self.customContextMenuRequested.connect(self._context_menu) # 连接customContextMenuRequested信号到_context_menu槽函数
 
-    @Slot(QPoint)
+    @Slot(QPoint) # 右键菜单槽函数，在表格视图中右击时调用，根据当前选择的单元格显示不同的右键菜单选项。
     def _context_menu(self, pos):
-        context_menu = QMenu("Context menu", self)
-        if self.selectedIndexes():
+        context_menu = QMenu("Context menu", self) # 创建一个QMenu对象作为右键菜单
+        if self.selectedIndexes(): # 据当前是否有选中的单元格来决定是否添加"Copy"动作
             copy_action = QAction("Copy", self)
-            copy_action.triggered.connect(self.copy_row)
-            context_menu.addAction(copy_action)
+            copy_action.triggered.connect(self.copy_row) # 将其triggered信号连接到copy_row槽函数
+            context_menu.addAction(copy_action) # 将"copy_action"添加到右键菜单context_menu中
 
         select_all_action = QAction("Select all", self)
         select_all_action.triggered.connect(self.selectAll)
-        context_menu.addAction(select_all_action)
-        context_menu.exec(self.mapToGlobal(pos))
+        context_menu.addAction(select_all_action) #  将"select_all_action"添加到右键菜单context_menu中
+        context_menu.exec(self.mapToGlobal(pos)) # 调用context_menu.exec(self.mapToGlobal(pos))来在指定位置显示右键菜单
 
+    """
+    设置表格模型。 可根据模型的列数 动态设置 表格视图的每列每列宽度
+
+    参数:
+    - model: 表格模型。
+    """
     def set_model(self, model):
-        super().setModel(model)
-        for i in range(0, model.columnCount()):
-            size = model.headerData(i, Qt.Horizontal, Qt.SizeHintRole)
-            self.setColumnWidth(i, size.width())
+        super().setModel(model) # 将参数model作为表格视图的模型
+        for i in range(0, model.columnCount()): # 循环遍历模型的列数
+            size = model.headerData(i, Qt.Horizontal, Qt.SizeHintRole) # 获取标题的大小提示
+            self.setColumnWidth(i, size.width()) # 设置每列的宽度
 
+    """
+    键盘按键事件处理函数。
+
+    参数:
+    - event: 键盘事件对象。
+
+    if  elif  else
+    """
     def keyPressEvent(self, event):
         if event.matches(QKeySequence.Copy):
             self.copy_row()
@@ -42,13 +58,16 @@ class ReceivedFramesView(QTableView):
         else:
             super().keyPressEvent(event)
 
-    @Slot()
+    """
+    复制选中行的内容到剪贴板。(复制选中的行的内容 并 粘贴到 其他地方)
+    """
+    @Slot() 
     def copy_row(self):
-        clipboard = QApplication.clipboard()
-        str_row = ""
-        last_column = self.model().columnCount() - 1
-        for index in self.selectedIndexes():
-            str_row += index.data(clipboard_text_role) + " "
+        clipboard = QApplication.clipboard() # 获取应用程序的剪贴板对象
+        str_row = ""   # 定义一个空字符串str_row来存储复制的行内容
+        last_column = self.model().columnCount() - 1  # 获取模型的最后一列索引号
+        for index in self.selectedIndexes():   # 过遍历选中的单元格
+            str_row += index.data(clipboard_text_role) + " "    # 将单元格的数据逐个添加到str_row中，并在每行的末尾添加一个换行符\n。
             if index.column() == last_column:
                 str_row += "\n"
-        clipboard.setText(str_row)
+        clipboard.setText(str_row)  # 将str_row的内容设置到剪贴板
